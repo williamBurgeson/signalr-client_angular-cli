ebfb673ee553838430ee623f1707e71d84c22f1f
William Burgeson <williamburgeson@Williams-MacBook-Pro-2.local>
2017-08-29 10:31:07 +0100

    Upgraded signalR (and ng-cli) to latest version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TransferMode, HttpClient } from 'signalr-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  ngOnInit() {
+    //console.log(TransferMode.Binary);
+    var http = new HttpClient();
+  }
 }
